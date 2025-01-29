1f4b27b3e7c3aafd285e7b92fa8899eb26087e0f
Ryan Ronish <roni0210@bears.unco.edu>
2025-01-29 16:50:22 -0700

    Transfer code from other project into new repo/project

---
+++ Shelf.App.BACS488/Components/InputView.swift
@@ -0,0 +1,18 @@
+//
+//  InputView.swift
+//  Shelf.App.BACS488
+//
+//  Created by Ryan Ronish on 1/29/25.
+//
+
+import SwiftUI
+
+struct InputView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    InputView()
+}
